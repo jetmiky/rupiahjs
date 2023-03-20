30baa274376facb6f339b2ab1526140f3fbbabd8
Bima Jatmiko Abadi <bja.devs@gmail.com>
2023-03-21 00:44:05 +0700

    Generate Rupiah class

---
+++ src/Rupiah.ts
@@ -0,0 +1,56 @@
+import terbilang from "./terbilang";
+import { add as addSeparator } from "./separators";
+
+export default class Rupiah {
+  private _value: number;
+
+  constructor(nominal: number = 0) {
+    this._value = nominal;
+  }
+
+  /**
+   * Get real number of value stored.
+   * @returns {number} Real number of value stored.
+   */
+  get value(): number {
+    return this._value;
+  }
+
+  /**
+   * Get Indonesian language of value (terbilang).
+   * @returns {string} Number spoken in Indonesian language.
+   */
+  get terbilang(): string {
+    return "".concat(terbilang(this._value), " Rupiah");
+  }
+
+  /**
+   * Mathematic operation of addition on value.
+   * @param {number} args Number(s) which will be added.
+   * @returns {number} Calculation of addition.
+   */
+  add(...args: number[]): number {
+    args.forEach((value) => (this._value += value));
+    return this._value;
+  }
+
+  /**
+   * Mathematic operation of subtraction on value.
+   * @param {number} args Number(s) which will be subtracted.
+   * @returns {number} Calculation of subtraction.
+   */
+  subtract(...args: number[]): number {
+    args.forEach((value) => (this._value -= value));
+    return this._value;
+  }
+
+  /**
+   * Format value in Indonesian currency.
+   * @param {"IDR" | "Rp"} prefix Prefix in front of currency.
+   * @param {"dot" | "comma"} separator Character of thousand separators.
+   * @returns {string} Value in form of Indonesian currency.
+   */
+  getCurrency(prefix: "IDR" | "Rp" = "Rp", separator: "dot" | "comma" = "dot"): string {
+    return "".concat(prefix, addSeparator(this._value, separator));
+  }
+}
